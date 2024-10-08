7ed631d37be2c3b137729d4f72cec4be5a94c49b
Your Name <you@example.com>
2024-10-08 15:32:43 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,10 +1,31 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
+       """
         :type s: str
         :rtype: int
         """
-        pass
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        n = len(s)
+        last = roman_map[s[n-1]]
+        total = 0
+
+        for i in range(n-1):
+            if roman_map[s[i]] < roman_map[s[i+1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+
+        total += last
+        return total
 
 
 
