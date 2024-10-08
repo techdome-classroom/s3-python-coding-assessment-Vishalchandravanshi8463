182f409a6b64536a56aa08c1a68e163988c504b5
Your Name <you@example.com>
2024-10-08 15:25:41 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,7 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        
+         stack = []
+
+        for ch in s:
+            if ch in '({[':
+                stack.append(ch)
+            else:
+                if len(stack) < 1:
+                    return False
+                if not self.check(stack, ch):
+                    return False
+
+        return len(stack) == 0
         pass
 
 
